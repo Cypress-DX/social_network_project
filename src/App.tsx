c7e38a495c715cfb775127ead36a946b682ca64a
Cypress-DX <dimisini@gmail.com>
2021-12-27 22:00:10 +0300

    Lesson 7 update

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+    return (
+        <div>
+            <a href="#ee">News </a>
+            <a href="#ee">Feed </a>
+            <a href="#ee">Content </a>
+            <a href="#ee">Messages</a>
+        </div>
+    )
+}
+
+const Technologies = () => {
+    return (
+        <div className="ul">
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
+
 const App = () => {
-  return (
-    <div className="ul">
-      <div className="random">Hello world!</div>
-      <div>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-        </ul>
-      </div>
-      <div>
-        <button>Click me</button>
-        <input placeholder="Type here"/>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            < Header/>
+            < Technologies/>
+        </div>
+)
+    ;
 }
 
 export default App;
