6e7f2d1c8ca4abf5acaa56fcff4406ba3a3ca7b6
Cypress-DX <dimisini@gmail.com>
2021-12-27 23:54:20 +0300

    Lesson 11 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Technologies} from "./Components/Technologies";
-import {Header} from "./Components/Header";
+
+
 
 
 
 const App = () => {
     return (
-        <div>
-            < Header/>
-            < Technologies/>
+        <div className='app-wrapper'>
+            <header className="header">
+                <img src="https://lh3.googleusercontent.com/proxy/xfAdf_QKjdhuT_QLA60YiWHD6JZCM7rjLBON9HmUTlXqb4Y0X58e3GYE1riO2atE_scHOhgv6nN56KBxhWQH1KonA_7O50RH3VF-AHrv3AYLPQ"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+                <div>
+                    News
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Settings
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src="https://thumbs.dreamstime.com/z/cyclists-bike-race-banner-style-colorful-illustration-bunch-taking-part-white-background-56476672.jpg"/>
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New Posts
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                        <div>
+                            Post 3
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 )
-    ;
 }
 
 export default App;
