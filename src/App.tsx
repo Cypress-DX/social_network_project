f583df1a9f9da27064c4cf5b6e136ec80f7876ee
Cypress-DX <dimisini@gmail.com>
2021-12-28 13:14:51 +0300

    Lesson 18 update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from "./Components/Header/Header";
 import {Navbar} from "./Components/Navbar/Navbar";
 import {Profile} from "./Components/Profile/Profile";
+import {Dialogs} from "./Components/Dialogs/Dialogs";
 
 
 
@@ -13,7 +14,10 @@ const App = () => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            <div className='app-wrapper-content'>
+                <Dialogs />
+            </div>
+            {/*<Profile/>*/}
         </div>
 )
 }
---
+++ src/Components/Dialogs/Dialogs.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Dialogs = () => {
+    return (
+        <header>
+            Dialogs
+        </header>
+    )
+}
