6706ccedc5bec2ab7ad642430e39693896b2f96c
Cypress-DX <dimisini@gmail.com>
2021-12-28 12:12:22 +0300

    Lesson 16 update

--- src/Components/Profile/Posts/Post/Post.tsx
+++ src/Components/Profile/Posts/Post/Post.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import s from "./Post.module.css";
 
-export const Post = () => {
+type MessageType = {
+    message: string
+    likes: number
+}
+
+export const Post = (props: MessageType) => {
     return (
         <div className={s.item}>
                 <img src="https://cdn.dribbble.com/users/1329247/screenshots/8195114/media/74d8d004178a94808265d8958c19c164.jpg?compress=1&resize=400x300"/>
-                Post 1
+            {props.message}
             <div>
-                <span>like</span>
+                <span>likes: {props.likes}</span>
             </div>
         </div>
 
--- src/Components/Profile/Posts/Posts.tsx
+++ src/Components/Profile/Posts/Posts.tsx
@@ -9,9 +9,12 @@ export const Posts = () => {
                 <div>
                     <textarea/>
                     <button>Add Post</button>
-                    <Post/>
-                    <Post/>
-                    <Post/>
+                    <Post
+                        message = "Hi, how are you?"
+                          likes = {4}
+                        />
+                    <Post message = "It is my second post" likes = {3}/>
+                    <Post message = "I am learning React" likes = {0}/>
                 </div>
         </div>
     )
